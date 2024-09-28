7f8875bd5580e083405ab7f867157fcce7194a16
abilash1410 <abilash1410@gmail.com>
2024-09-28 10:06:15 +0530

    Delete constants.js

--- constants.js
+++
@@ -1,2 +0,0 @@
-export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-withlettuce-3624ld.png";
-export const CLOUDINARY_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_292,h_300/";
